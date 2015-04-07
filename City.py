085eae8b302e5e2a35a59955763dafb0d210b655
Tony <CodeGuro@gmail.com>
2015-04-07 14:58:39 -0400

    updates

---
+++ City.py
@@ -0,0 +1,16 @@
+__author__ = 'Tony'
+
+
+# This class is a data structure used to generalize cities for the Gis class
+class City:
+
+    def __init__(self, name, state, latitude, longitude, population):
+        self.name = name
+        self.state = state
+        self.latitude = latitude
+        self.longitude = longitude
+        self.population = population
+        self.cities = {}
+
+    def appendDistanceTo(self, name, distance):
+        self.cities[name] = distance
--- GIS.py
+++ GIS.py
@@ -1,15 +1,37 @@
 __author__ = 'Tony'
 
+from City import City
+import re
+
 
 class Gis:
+
     def __init__(self):
-        self.x = 5
-        self.y = 32
+        self.cities = []  # This is the list of all the available cities
+        with open('gis.dat', 'r') as gisfile:
+            gisfile.seek(110)
+            tokens = re.split(", |[\s+\[\],]", gisfile.read())
+            i = 0  # iterator
+            while i < len(tokens):
+                name = tokens[i+0]
+                state = tokens[i+1]
+                lat = tokens[i+2]
+                lon = tokens[i+3]
+                pop = tokens[i+4]
+                i += 5
+                if i == 205:
+                    print('Stop here!')
+                current_city = City(name, state, lat, lon, pop)
+                for city in self.cities:
+                    city.appendDistanceTo(name, int(tokens[i]))
+                    i += 1
+                current_city.appendDistanceTo(name, 0)
+                self.cities.append(current_city)
 
-    def printham(self):
-        print('ham')
+            pass
+        print('success!')
 
-    def selectCities(self, attribute, lowerBound, upperBound ):
+    def selectCities(self, attribute, lowerBound, upperBound):
         pass
 
     def selectAllCities(self):
@@ -39,6 +61,4 @@ class Gis:
     def minCut(self):
         pass
 
-
 x = Gis()
-x.printham()
